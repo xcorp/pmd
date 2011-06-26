e2b1b7f2103fa39c9924ddd6a9b6563725993d58
Nisse Karlsson <nisse@xcorp.at>
2011-06-26 16:11:50 +0200

    Fixed the SQL query when inserting actor-movie, genre-movie etc. And added the file :)

--- main.php
+++ main.php
@@ -13,37 +13,40 @@ function escapeSpaces($string) {
 }
 
 
-function insertMovie($data) {
+function insertMovie($movie) {
 	global $mysqli;
-	if(checkIfMovieExists($data->ID)) {
+	if(checkIfMovieExists($movie->ID)) {
 		echo "Movie already exists";
 	}
 	else {
 		$query = 'INSERT INTO movies VALUES (
-			"'.$data->ID.'",
-			"'.$data->Title.'",
-			"'.$data->Rated.'",
-			"'.$data->Year.'",
-			"'.$data->Released.'",
-			"'.$data->Poster.'",
-			"'.$data->Runtime.'",
-			"'.$data->Plot.'");';
-		foreach($data->Genre as $genre) {
+			"'.$movie->ID.'",
+			"'.$movie->Title.'",
+			"'.$movie->Rated.'",
+			"'.$movie->Year.'",
+			"'.$movie->Released.'",
+			"'.$movie->Poster.'",
+			"'.$movie->Runtime.'",
+			"'.$movie->Plot.'");';
+		foreach($movie->Genre as $genre) {
 			$query .= 'INSERT INTO genres (genre) VALUES ("'.$genre.'")
- 						on duplicate key update id=id; ';
+ 						on duplicate key update id=id;
+ 						INSERT INTO `genre-movie` SELECT id, "'.$movie->ID.'" FROM genres  WHERE genre="'.$genre.'";';
 		}
-		foreach($data->Director as $director) {
+		foreach($movie->Director as $director) {
 			$query .= 'INSERT INTO directors (director) VALUES ("'.$director.'")
  						on duplicate key update id=id;
- 						INSERT INTO director-movie VALUES ((SELECT id FROM director WHERE director="'.$director.'"), '.$data->ID.');';
+ 						INSERT INTO `director-movie` SELECT id, "'.$movie->ID.'" FROM directors  WHERE director="'.$director.'";';
 		}
-		foreach($data->Writer as $writer) {
+		foreach($movie->Writer as $writer) {
 			$query .= 'INSERT INTO writers (writer) VALUES ("'.$writer.'")
- 						on duplicate key update id=id; ';
+ 						on duplicate key update id=id;
+ 						INSERT INTO `writer-movie` SELECT id, "'.$movie->ID.'" FROM writers  WHERE writer="'.$writer.'";';
 		}
-		foreach($data->Actors as $actor) {
+		foreach($movie->Actors as $actor) {
 			$query .= 'INSERT INTO actors (actor) VALUES ("'.$actor.'")
- 						on duplicate key update id=id; ';
+ 						on duplicate key update id=id;
+ 						INSERT INTO `actor-movie` SELECT id, "'.$movie->ID.'" FROM actors  WHERE actor="'.$actor.'";';
 		}
 		
 		echo $query;
